c2692ba73535fe44a39ee9726ee1c471be044b3d
syed-dce <syed7771@yahoo.com>
2025-04-18 12:26:42 +0530

    STM32 TouchGFX

---
+++ README.md
@@ -0,0 +1,23 @@
+# Waveshare 7" Capacitive Touch Screen 800x480 + STM32F429ZI + touchGFX
+
+## Equipment
+### STM32F429ZI Microcontroller
+- 2Mb of Flash Memory 
+- 256Kb of RAM
+- Up to 180Mhz clock speed
+- Chrom-ART accelerator built around a 2D DMA engin
+- Embedded LCD-TFT Display Controller __(LTDC)__ (that we didn't use because we don't have the right display)
+- Flexible Memory Controller __(FMC)__ (to interface with external memories)
+
+### Waveshare 7" Touch Screen
+- 800x480 resolution
+- 16 bits colors
+- Embedded Display Controller __(RA8875)__
+- Embedded Touch Controller __(GT811)__
+
+## Software tools
+- TouchGFX for GUI development
+
+## Functional Block Diagram
+![functional block diagram](HW.png)
+
---
+++ stm32f429ZI/Core/Inc/lcd_TP.h
@@ -0,0 +1,36 @@
+/*
+ ******************************************************************************
+ *
+ *   This program is a free software: you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the Free
+ *   Software Foundation, either version 3 of the License, or any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *   more details.
+ *                   <http://www.gnu.org/licenses/>.
+ *
+ *******************************************************************************
+*/
+
+
+
+#ifndef INC_LCD_TP_H_
+#define INC_LCD_TP_H_
+
+
+#include "main.h"
+
+#define LCD_TP_ADDRESS 0xBA
+#define LCD_TP_REGISTERS_CONFIGURATION 0x6A2
+#define LCD_TP_REGISTERS_ALL_DATA 0x721
+#define LCD_TP_REGISTERS_STATUS 0x721
+#define LCD_TP_REGISTERS_DATA_T1 0x723
+
+
+uint8_t LCD_TP_init(void);
+uint8_t LCD_TP_getStatus(void);
+void LCD_TP_getT1(uint16_t* X, uint16_t* Y);
+
+#endif /* INC_LCD_TP_H_ */
